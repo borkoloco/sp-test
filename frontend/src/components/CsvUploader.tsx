be51561215768fd8ba00719aea2b760b610c8171
borkoloco <santiagobosca@gmail.com>
2024-09-12 00:58:17 -0300

    clean

--- frontend/src/components/CsvUploader.tsx
+++ frontend/src/components/CsvUploader.tsx
@@ -21,7 +21,6 @@ const CsvUploader: React.FC = () => {
   const [fileInvalid, setFileInvalid] = useState<boolean>(false);
   const [messageError, setMessageHere] = useState<string>("");
 
-  // La URL del backend
   const url = process.env.REACT_APP_API_URL;
 
   const uploadFile = async () => {
@@ -38,11 +37,11 @@ const CsvUploader: React.FC = () => {
 
         if (response.status === 200) {
           await fetchUsersData();
-          setFileInvalid(false); // Si el archivo se subió correctamente, no hay error
+          setFileInvalid(false);
         }
       }
     } catch (error: any) {
-      handleFileUploadError(error); // Si hay error, lo manejamos
+      handleFileUploadError(error);
     }
   };
 
@@ -58,14 +57,12 @@ const CsvUploader: React.FC = () => {
     }
   };
 
-  // Memoriza handleFetchDataError para evitar recrearlo en cada render
   const handleFetchDataError = useCallback((error: any) => {
     if (error.response && error.response.status === 400) {
       handleFileUploadError(error.response.data.message);
     }
-  }, []); // No tiene dependencias que necesiten ser actualizadas
+  }, []);
 
-  // Memoriza fetchUsersData y agrega handleFetchDataError como dependencia
   const fetchUsersData = useCallback(async () => {
     try {
       const response = await axios.get(`${url}/api/users?q=${searchTerm}`);
@@ -73,14 +70,12 @@ const CsvUploader: React.FC = () => {
     } catch (error: any) {
       handleFetchDataError(error);
     }
-  }, [url, searchTerm, handleFetchDataError]); // handleFetchDataError agregado como dependencia
-
+  }, [url, searchTerm, handleFetchDataError]);
   const handleFileUploadError = (error: any) => {
     if (error.response) {
       console.error("Error status:", error.response.status);
       console.error("Error data:", error.response.data);
 
-      // Aquí capturamos el mensaje que devuelve el backend
       setMessageHere(
         error.response.data.message || "Upload failed due to an unknown error."
       );
@@ -91,12 +86,11 @@ const CsvUploader: React.FC = () => {
       console.error("Error message:", error.message);
       setMessageHere(error.message);
     }
-    setFileInvalid(true); // Marca que el archivo es inválido
+    setFileInvalid(true);
   };
 
-  // Incluye fetchUsersData en la lista de dependencias
   useEffect(() => {
-    fetchUsersData(); // Se llama cuando searchTerm cambia
+    fetchUsersData();
   }, [fetchUsersData]);
 
   return (
@@ -153,164 +147,3 @@ const CsvUploader: React.FC = () => {
 };
 
 export default CsvUploader;
-
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import { Button, Card } from "react-bootstrap";
-// import SearchBar from "./SearchBar";
-// import FileUploader from "./FileUploader";
-// import FileInvalidMessage from "./FileInvalidMessage";
-// import {
-//   Container,
-//   Bar,
-//   Toolbar,
-//   Title,
-//   Body,
-//   CardWrapper,
-//   Div,
-// } from "./style/StyledComponents";
-
-// const CsvUploader: React.FC = () => {
-//   const [file, setFile] = useState<File | null>(null);
-//   const [csvData, setCsvData] = useState<any[]>([]);
-//   const [searchTerm, setSearchTerm] = useState<string>("");
-//   const [fileInvalid, setFileInvalid] = useState<boolean>(false);
-//   const [messageError, setMessageHere] = useState<string>("");
-
-//   // const url = "http://localhost:3000";
-//   const url = process.env.REACT_APP_API_URL;
-
-//   const uploadFile = async () => {
-//     try {
-//       if (file) {
-//         const formData = new FormData();
-//         formData.append("file", file);
-
-//         console.log(file); // Verifica que el archivo está siendo seleccionado
-//         console.log(formData); // Verifica que el FormData se está creando correctamente
-
-//         const response = await axios.post(`${url}/api/files`, formData, {
-//           headers: {
-//             "Content-Type": "multipart/form-data",
-//           },
-//         });
-
-//         if (response.status === 200) {
-//           await fetchUsersData();
-//           setFileInvalid(false);
-//         } else {
-//           handleFileUploadError(response.data.message);
-//         }
-//       }
-//     } catch (error: any) {
-//       handleFileUploadError(error.message);
-//     }
-//   };
-
-//   const clearData = async () => {
-//     try {
-//       const response = await axios.delete(`${url}/api/clear`);
-//       alert("Data cleared successfully");
-//       if (response.status === 200) {
-//         setCsvData([]);
-//       }
-//     } catch (error: any) {
-//       handleFetchDataError(error);
-//     }
-//   };
-
-//   const fetchUsersData = async () => {
-//     try {
-//       const response = await axios.get(`${url}/api/users?q=${searchTerm}`);
-//       setCsvData(response.data.data || []);
-//     } catch (error: any) {
-//       handleFetchDataError(error);
-//     }
-//   };
-
-//   // const handleFileUploadError = (errorMessage: string) => {
-//   //   setMessageHere(errorMessage);
-//   //   setFileInvalid(true);
-//   // };
-//   const handleFileUploadError = (error: any) => {
-//     if (error.response) {
-//       // Si el servidor responde con un error, muestra el mensaje del servidor
-//       console.error("Error status:", error.response.status);
-//       console.error("Error data:", error.response.data);
-//       setMessageHere(
-//         error.response.data.message || "Upload failed due to an unknown error."
-//       );
-//     } else if (error.request) {
-//       // Si no hay respuesta del servidor
-//       console.error("Error request:", error.request);
-//       setMessageHere("No response received from server. Please try again.");
-//     } else {
-//       // Otros errores
-//       console.error("Error message:", error.message);
-//       setMessageHere(error.message);
-//     }
-//     setFileInvalid(true);
-//   };
-
-//   const handleFetchDataError = (error: any) => {
-//     if (error.response && error.response.status === 400) {
-//       handleFileUploadError(error.response.data.message);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchUsersData();
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [searchTerm]);
-
-//   return (
-//     <Container>
-//       <Toolbar>
-//         <Bar>
-//           <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
-//         </Bar>
-
-//         <Title>
-//           <h5>SP-Test</h5>
-//         </Title>
-//         <Div>
-//           <FileUploader onFileChange={setFile} />
-//           <Button
-//             variant="primary"
-//             onClick={uploadFile}
-//             data-testid="upload-button"
-//             disabled={!file}
-//           >
-//             Upload
-//           </Button>
-//           <Button
-//             variant="danger"
-//             onClick={clearData}
-//             data-testid="clear-button"
-//             disabled={csvData.length === 0}
-//           >
-//             Clear
-//           </Button>
-//         </Div>
-//       </Toolbar>
-
-//       {fileInvalid && <FileInvalidMessage errorMessage={messageError} />}
-
-//       <Body>
-//         {csvData.map((row, index) => (
-//           <CardWrapper key={index}>
-//             <Card.Body>
-//               {Object.entries(row).map(([key, value]) => (
-//                 <p key={key}>
-//                   <strong>{key}: </strong> {String(value)}
-//                 </p>
-//               ))}
-//             </Card.Body>
-//           </CardWrapper>
-//         ))}
-//       </Body>
-//     </Container>
-//   );
-// };
-
-// export default CsvUploader;
