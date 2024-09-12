148aca2e483a459f02429fd3f509845f69d1a3dc
borkoloco <santiagobosca@gmail.com>
2024-09-12 00:41:43 -0300

    front

--- frontend/src/components/FileInvalidMessage.tsx
+++ frontend/src/components/FileInvalidMessage.tsx
@@ -7,9 +7,7 @@ interface FileInvalidMessageProps {
 const FileInvalidMessage: React.FC<FileInvalidMessageProps> = ({
   errorMessage,
 }) => {
-  // No renderiza nada si el errorMessage está vacío
-  if (!errorMessage) return null;
-
+  if (!errorMessage) return null; // Si no hay mensaje, no mostramos nada
   return (
     <div className="alert alert-danger" role="alert">
       {errorMessage}
@@ -18,6 +16,27 @@ const FileInvalidMessage: React.FC<FileInvalidMessageProps> = ({
 };
 
 export default FileInvalidMessage;
+
+// import React from "react";
+
+// interface FileInvalidMessageProps {
+//   errorMessage: string;
+// }
+
+// const FileInvalidMessage: React.FC<FileInvalidMessageProps> = ({
+//   errorMessage,
+// }) => {
+//   // No renderiza nada si el errorMessage está vacío
+//   if (!errorMessage) return null;
+
+//   return (
+//     <div className="alert alert-danger" role="alert">
+//       {errorMessage}
+//     </div>
+//   );
+// };
+
+// export default FileInvalidMessage;
 
 // import React from "react";
 
